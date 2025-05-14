5d3e9ed28fce8151a43a5669de4d430b24c79a29
bushrareguieg <83142468+bushrareguieg@users.noreply.github.com>
2025-05-14 23:34:49 +0200

    Update script.js

--- script.js
+++ script.js
@@ -11,10 +11,11 @@ window.onload = function() {
     // Reveal the apology message
     setTimeout(() => {
         document.querySelector(".message").style.opacity = 1;
-        document.getElementById("textMessage").innerText = 
-            "I am sorry, my love.\nI love you more than words can say.";
-            "I’ve been thinking a lot about what you said, and it really touched me. I’m so sorry if it feels like we’ve been distant lately, like two mountains standing far apart. I never want you to feel that way.\n\n" +
-            "Please know that no matter how far it seems, you’re always close to my heart.I want to make sure you always feel my love, no matter the distance.\n\n" +
+        document.getElementById("textMessage").innerHTML = 
+            "I am sorry, my love.<br>" +
+            "I love you more than words can say.<br><br>" +
+            "I’ve been thinking a lot about what you said, and it really touched me. I’m so sorry if it feels like we’ve been distant lately, like two mountains standing far apart. I never want you to feel that way.<br><br>" +
+            "Please know that no matter how far it seems, you’re always close to my heart. I want to make sure you always feel my love, no matter the distance.<br><br>" +
             "Let’s bridge that gap, just the two of us, and bring back that closeness we cherish. I miss you, and I promise I’ll do better.";
     }, 2000);
 };
